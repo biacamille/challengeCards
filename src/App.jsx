d568d502936d7da7c71fe139381bc0f3c9e5a806
Beatriz Camille <biacamille2019@gmail.com>
2022-08-13 12:05:24 -0300

    botões e cards criados

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useEffect } from 'react'
+import { useEffect , useState } from 'react'
 import './App.css'
 
 function App() {
-   
+  const[clientDate, setClientDate] = useState([]);
+  const[totalClient, setTotalClient] = useState([]);
+  const[isShowingResult, setIsShowingResult] = useState(false);
+
   useEffect(() =>{
+
+  fetch('https://api-staging.vagalumewifi.com.br/api/tests/infoTeste')
+  .then(response => response.json())
+  .then(data => {
+    setTotalClient(data)
+  })
+    
    fetch('https://api-staging.vagalumewifi.com.br/api/tests/apiTeste')
    .then(response => response.json())
    .then(data => {
- 
+
       const conectionInTheDate = data.connected.filter(item => {
 
         let date = item.last_connection.split("/");
@@ -20,46 +30,51 @@ function App() {
         return item
        }})
        
-       let user_id
+       let user_id = [];
        const clientInTheDate = conectionInTheDate.filter(item =>{
-         if(item.client_id === 'vagalume' && item.user_id !== user_id){
-           user_id = item.user_id
+         if(item.client_id === 'vagalume' && !user_id.includes(item.user_id)){
+           user_id.push(item.user_id)
            return item
           }
         })
-        console.log(clientInTheDate)
-
-    //      return clientInTheDate
-    //    }
-    //       console.log(client_id)
-     
-  //   });
-    })
+        setClientDate(clientInTheDate)
+      })
        
 
   },[])
-  
+ 
+function handleShowingTheCards(){
+  setIsShowingResult(!isShowingResult);
+}
+
   return (
   <div id="cards">
-   <div className="card">
+    {isShowingResult && (
+      <>
+      <div className="card">
        <h1>Pessoas Totais</h1>
-      <p>500</p>	
+      <p>{totalClient.total}</p>	
     </div>
     <div className="card">
       <h1>Pessoas Online</h1>
-      <p> 30</p>	
+      <p>{totalClient.online}</p>	
     </div>
     <div className="card">
       <h1>Pessoas Offline</h1>
-      <p>300 </p>
+      <p>{totalClient.total - totalClient.online}</p>
     </div>
       <div className="card">
-        <h1>Pessoas Conectadas no período: </h1>
-        <p>50</p>
-
+        <h1>Pessoas Conectadas no período</h1>
+        <p>{clientDate.length}</p>
       </div>
-    </div>
-
+      </>
+    )}
+   
+      <div>
+        <button className="button" onClick={handleShowingTheCards}>Ver resultados</button>
+      </div>
+    
+      </div>
 
 )
 }
